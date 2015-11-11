997b7549a7b4d1f8837fbecc28d12229d9516ef4
alithephantom <alithephantom@gmail.com>
2015-11-11 18:42:29 +0200

    the server capturing now works in it's own thread

--- src/VUServer.java
+++ src/VUServer.java
@@ -10,7 +10,7 @@ public class VUServer {
     AudioInputStream InputStream;
     SourceDataLine sourceLine;
     boolean stopaudioCapture = false;
-
+    int portNumber;
     private AudioFormat getAudioFormat() {
         float sampleRate = 16000.0F;
         int sampleInbits = 16;
@@ -25,33 +25,9 @@ public class VUServer {
     }
 
     public void runVOIP(int portNumber) {
-        try {
-            DatagramSocket serverSocket = new DatagramSocket(portNumber);
-            System.out.println("server is running");
-            byte[] receiveData = new byte[10000];
-            while (true) {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-                System.out.println("RECEIVED: " + receivePacket.getAddress().getHostAddress() + " " + receivePacket.getPort());
-                try {
-                    byte audioData[] = receivePacket.getData();
-                    InputStream byteInputStream = new ByteArrayInputStream(audioData);
-                    AudioFormat adFormat = getAudioFormat();
-                    InputStream = new AudioInputStream(byteInputStream, adFormat, audioData.length / adFormat.getFrameSize());
-                    DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, adFormat);
-                    sourceLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-                    sourceLine.open(adFormat);
-                    sourceLine.start();
-                    Thread playThread = new Thread(new PlayThread());
-                    playThread.start();
-                } catch (Exception e) {
-                    System.out.println(e);
-                    System.exit(0);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.portNumber = portNumber;
+        Thread receiveThread = new Thread(new RecieveThread());
+        receiveThread.start();
     }
 
     class PlayThread extends Thread {
@@ -67,37 +43,45 @@ public class VUServer {
 
                     }
                 }
-                //  sourceLine.drain();
-                // sourceLine.close();
+                //sourceLine.drain();
+                //sourceLine.close();
             } catch (Exception e) {
                 System.out.println(e);
                 System.exit(0);
             }
         }
     }
-    class CaptureThread extends Thread {
 
-        byte tempBuffer[] = new byte[10000];
+    class RecieveThread extends Thread {
         public void run() {
-
-            byteOutputStream = new ByteArrayOutputStream();
-            stopaudioCapture = false;
             try {
-                DatagramSocket clientSocket = new DatagramSocket(8786);
-                InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
-                while (!stopaudioCapture) {
-                    int cnt = targetDataLine.read(tempBuffer, 0, tempBuffer.length);
-                    if (cnt > 0) {
-                        DatagramPacket sendPacket = new DatagramPacket(tempBuffer, tempBuffer.length, IPAddress, 9786);
-                        clientSocket.send(sendPacket);
-                        byteOutputStream.write(tempBuffer, 0, cnt);
+                DatagramSocket serverSocket = new DatagramSocket(portNumber);
+                System.out.println("server is running");
+                byte[] receiveData = new byte[10000];
+                while (true) {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+                    System.out.println("RECEIVED: " + receivePacket.getAddress().getHostAddress() + " " + receivePacket.getPort());
+                    try {
+                        byte audioData[] = receivePacket.getData();
+                        InputStream byteInputStream = new ByteArrayInputStream(audioData);
+                        AudioFormat adFormat = getAudioFormat();
+                        InputStream = new AudioInputStream(byteInputStream, adFormat, audioData.length / adFormat.getFrameSize());
+                        DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, adFormat);
+                        sourceLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+                        sourceLine.open(adFormat);
+                        sourceLine.start();
+                        Thread playThread = new Thread(new PlayThread());
+                        playThread.start();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        System.exit(0);
                     }
                 }
-                byteOutputStream.close();
             } catch (Exception e) {
-                System.out.println("CaptureThread::run()" + e);
-                System.exit(0);
+                e.printStackTrace();
             }
         }
+
     }
 }
